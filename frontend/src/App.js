ad8cd4e43411f61e6a602b746fdde36ad29be41b
DavidC1287 <davidcontreras1287@gmail.com>
2024-03-11 21:13:47 -0700

    commit 3/11

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './image.png';
 import './App.css';
+import axios from 'axios'; // Import Axios for making HTTP requests
+import LoginPage from './loginpage';
 
 function App() {
+  const [loginData, setLoginData] = useState({ username: '', password: '' });
+
+  const handleLogin = () => {
+    // Make a POST request to your backend with loginData
+    axios.post('http://localhost:8080/login', loginData)
+      .then(response => {
+        // Handle successful login response
+        console.log('Login successful:', response.data);
+      })
+      .catch(error => {
+        // Handle login error
+        console.error('Login error:', error);
+      });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData({ ...loginData, [name]: value });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome To Sub-Tracker</h1>
+        <p>Manage all your subscriptions in one place.</p>
+        <LoginPage
+          loginData={loginData}
+          onInputChange={handleInputChange}
+          onLogin={handleLogin}
+        />
       </header>
     </div>
   );
